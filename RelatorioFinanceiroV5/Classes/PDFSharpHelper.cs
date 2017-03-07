73c888fdb8c3d26b108d2146513936ccfdaa8ebb
Gerson Cardoso Filho <gersoncfilho@mac.com>
2017-03-07 10:45:19 -0300

    Inclusao do relatorio em espanhol

--- RelatorioFinanceiroV5/Classes/PDFSharpHelper.cs
+++ RelatorioFinanceiroV5/Classes/PDFSharpHelper.cs
@@ -14,7 +14,9 @@ namespace RelatorioFinanceiroV5.Classes
 {
     public class PDFSharpHelper
     {
-        
+
+        static string texto1, texto2, texto3, texto4, texto5, texto6, texto7, texto8, texto9, texto10, texto11, texto12, texto13, texto14;
+        static string cabecalho;
 
         public static void CreatePDF(PDFGrupo pdfgrupo, int _classificacao)
         {
@@ -30,10 +32,40 @@ namespace RelatorioFinanceiroV5.Classes
             if (_classificacao == 1)
             {
                 _local = "pt-BR";
+                texto1 = "Relatório Financeiro - Nuvem de Livros";
+                texto2 = "Número de Ítens da Editora";
+                texto3 = "Quantidade de Conteúdos";
+                texto4 = "% da editora do total";
+                texto5 = "Número de Ítens da Editora";
+                texto6 = "Conteúdo de Ref e Mais Acessados";
+                texto7 = "% da editora dos 10% mais acessados e ref.";
+                texto8 = "Receita líquida total da Nuvem de Livros";
+                texto9 = "Receita a ser dividida entre as editoras pelo conteúdo(20%)";
+                texto10 = "Receita a ser dividida pelas obras de ref e mais acessados";
+                texto11 = "Receita total a ser dividida entre as editoras";
+                texto12 = "Repasse pela quantidade de conteúdos";
+                texto13 = "Repasse pelas obras de ref e mais acessados";
+                texto14 = "Repasse total";
+                cabecalho = @"C:\Users\Gerson\Documents\repos\DOTNet\RelatorioFinanceiroV5\RelatorioFinanceiroV5\Images\cabecalho.png";
             }
             else if (_classificacao == 2)
             {
                 _local = "en-US";
+                texto1 = "Informe Financiero – Nube de Libros";
+                texto2 = "Número de Artículos de la Editorial";
+                texto3 = "Cantidad de Contenidos";
+                texto4 = "% de la editorial del total";
+                texto5 = "Número de Artículos de la Editorial";
+                texto6 = "Contenido de Ref y Más Consultados";
+                texto7 = "% de la editorial del 10% más consultado y ref";
+                texto8 = "Ingresos netos totales de Nube de Libros";
+                texto9 = "Ingresos que se dividirán entre las editoriales por el contenido (el 20%)";
+                texto10 = "Ingresos que se dividirán por las obras de ref y más consultados";
+                texto11 = "Ingresos totales que se dividirán entre las editoriales";
+                texto12 = "Transferencia por la cantidad de contenidos";
+                texto13 = "Transferencia por las obras de ref y más consultados";
+                texto14 = "Transferencia total";
+                cabecalho = @"C:\Users\Gerson\Documents\repos\DOTNet\RelatorioFinanceiroV5\RelatorioFinanceiroV5\Images\cabecalhoesp.png";
             }
 
             PdfDocument document = new PdfDocument();
@@ -56,31 +88,31 @@ namespace RelatorioFinanceiroV5.Classes
 
             //Draw header
             XRect rectHeader = new XRect(10, 10, 575, 165);
-            XImage image = XImage.FromFile(@"C:\Users\Gerson\Documents\repos\DOTNet\RelatorioFinanceiroV5\RelatorioFinanceiroV5\Images\cabecalho.png");
+            XImage image = XImage.FromFile(cabecalho);
             double x = (250 - image.PixelWidth * 72 / image.HorizontalResolution) / 2;
             gfx.DrawImage(image, 10, 10);
 
             //Título relatorio
             XRect rectTitulo = new XRect(11, 155, 572, 40);
             gfx.DrawRectangle(XBrushes.CornflowerBlue, rectTitulo);
-            gfx.DrawString("Relatório Financeiro - Nuvem de Livros", font, XBrushes.White, rectTitulo, XStringFormats.Center);
+            gfx.DrawString(texto1, font, XBrushes.White, rectTitulo, XStringFormats.Center);
 
             //Grupo e mes de referencia
-            XRect rectGrupo = new XRect(11, 195,440,20);
+            XRect rectGrupo = new XRect(11, 195, 440, 20);
             gfx.DrawRectangle(XBrushes.LightBlue, rectGrupo);
             gfx.DrawString(pdfgrupo.Grupo, font12, XBrushes.White, rectGrupo, XStringFormats.TopLeft);
 
-            XRect rectMesReferencia = new XRect(441,195,142,20);
+            XRect rectMesReferencia = new XRect(441, 195, 142, 20);
             gfx.DrawRectangle(XBrushes.LightBlue, rectMesReferencia);
             gfx.DrawString(pdfgrupo.MesReferencia, font12, XBrushes.White, rectMesReferencia, XStringFormats.TopLeft);
 
-            XRect rectTitulo2 = new XRect(11,215,572,20);
+            XRect rectTitulo2 = new XRect(11, 215, 572, 20);
             //gfx.DrawRectangle(XPens.Black, rectTitulo2);
-            gfx.DrawString("Número de Ítens da Editora", font12Bold, XBrushes.Black, rectTitulo2, XStringFormats.TopLeft);
+            gfx.DrawString(texto2, font12Bold, XBrushes.Black, rectTitulo2, XStringFormats.TopLeft);
 
             XRect rectQuantConteudo = new XRect(11, 235, 440, 20);
             //gfx.DrawRectangle(XPens.Black, rectQuantConteudo);
-            gfx.DrawString("Quantidade de Conteúdos", font12, XBrushes.Black, rectQuantConteudo, XStringFormats.TopLeft);
+            gfx.DrawString(texto3, font12, XBrushes.Black, rectQuantConteudo, XStringFormats.TopLeft);
 
             XRect rectQuantidade = new XRect(441, 235, 142, 20);
             //gfx.DrawRectangle(XBrushes.LightBlue, rectQuantidade);
@@ -88,7 +120,7 @@ namespace RelatorioFinanceiroV5.Classes
 
             XRect rectPercentualTotal = new XRect(11, 255, 440, 20);
             //gfx.DrawRectangle(XPens.Black, rectPercentualTotal);
-            gfx.DrawString("% da editora do total", font12, XBrushes.Black, rectPercentualTotal, XStringFormats.TopLeft);
+            gfx.DrawString(texto4, font12, XBrushes.Black, rectPercentualTotal, XStringFormats.TopLeft);
 
             XRect rectPercentual = new XRect(441, 255, 142, 20);
             //gfx.DrawRectangle(XBrushes.LightBlue, rectPercentual);
@@ -96,58 +128,58 @@ namespace RelatorioFinanceiroV5.Classes
 
             XRect rectTitulo3 = new XRect(11, 275, 572, 20);
             //gfx.DrawRectangle(XPens.Black, rectTitulo3);
-            gfx.DrawString("Número de Ítens da Editora", font12Bold, XBrushes.Black, rectTitulo3, XStringFormats.TopLeft);
+            gfx.DrawString(texto5, font12Bold, XBrushes.Black, rectTitulo3, XStringFormats.TopLeft);
 
             XRect rectContMaisAcessadosTitulo = new XRect(11, 295, 440, 20);
-            gfx.DrawString("Conteúdo de Ref e Mais Acessados", font12, XBrushes.Black, rectContMaisAcessadosTitulo, XStringFormats.TopLeft);
+            gfx.DrawString(texto6, font12, XBrushes.Black, rectContMaisAcessadosTitulo, XStringFormats.TopLeft);
 
             XRect rectContMaisAcessados = new XRect(441, 295, 142, 20);
             gfx.DrawString(pdfgrupo.QuantidadeMaisAcessados.ToString(), font12Bold, XBrushes.Black, rectContMaisAcessados, XStringFormats.TopLeft);
 
             XRect rectPercentualEditoraTitulo = new XRect(11, 315, 440, 20);
-            gfx.DrawString("% da editora dos 10% mais acessados e ref.", font12, XBrushes.Black, rectPercentualEditoraTitulo, XStringFormats.TopLeft);
+            gfx.DrawString(texto7, font12, XBrushes.Black, rectPercentualEditoraTitulo, XStringFormats.TopLeft);
 
             XRect rectPercentualEditora = new XRect(441, 315, 142, 20);
             gfx.DrawString(pdfgrupo.PercentualMaisAcessados.ToString(), font12Bold, XBrushes.Black, rectPercentualEditora, XStringFormats.TopLeft);
 
             XRect rectReceitaLiquidaTitulo = new XRect(11, 335, 440, 20);
-            gfx.DrawString("Receita líquida total da Nuvem de Livros", font12, XBrushes.Black, rectReceitaLiquidaTitulo, XStringFormats.TopLeft);
+            gfx.DrawString(texto8, font12, XBrushes.Black, rectReceitaLiquidaTitulo, XStringFormats.TopLeft);
 
             XRect rectReceitaLiquida = new XRect(441, 335, 142, 20);
             gfx.DrawString(receita.ToString("C2", CultureInfo.CreateSpecificCulture(_local)), font12Bold, XBrushes.Black, rectReceitaLiquida, XStringFormats.TopLeft);
 
             XRect rectReceita20Titulo = new XRect(11, 355, 440, 20);
-            gfx.DrawString("Receita a ser dividida entre as editoras pelo conteúdo(20%)", font12, XBrushes.Black, rectReceita20Titulo, XStringFormats.TopLeft);
+            gfx.DrawString(texto9, font12, XBrushes.Black, rectReceita20Titulo, XStringFormats.TopLeft);
 
             XRect rectReceita20 = new XRect(441, 355, 142, 20);
             gfx.DrawString(receita20.ToString("C2", CultureInfo.CreateSpecificCulture(_local)), font12Bold, XBrushes.Black, rectReceita20, XStringFormats.TopLeft);
 
             XRect rectReceita10Titulo = new XRect(11, 375, 440, 20);
-            gfx.DrawString("Receita a ser dividida pelas obras de ref e mais acessados", font12, XBrushes.Black, rectReceita10Titulo, XStringFormats.TopLeft);
+            gfx.DrawString(texto10, font12, XBrushes.Black, rectReceita10Titulo, XStringFormats.TopLeft);
 
             XRect rectReceita10 = new XRect(441, 375, 142, 20);
             gfx.DrawString(receita10.ToString("C2", CultureInfo.CreateSpecificCulture(_local)), font12Bold, XBrushes.Black, rectReceita10, XStringFormats.TopLeft);
 
             XRect rectReceita30Titulo = new XRect(11, 395, 440, 20);
-            gfx.DrawString("Receita total a ser dividida entre as editoras", font12, XBrushes.Black, rectReceita30Titulo, XStringFormats.TopLeft);
+            gfx.DrawString(texto11, font12, XBrushes.Black, rectReceita30Titulo, XStringFormats.TopLeft);
 
             XRect rectReceita30 = new XRect(441, 395, 142, 20);
             gfx.DrawString(receita30.ToString("C2", CultureInfo.CreateSpecificCulture(_local)), font12Bold, XBrushes.Black, rectReceita30, XStringFormats.TopLeft);
 
             XRect rectRepasseQuantidadeTitulo = new XRect(11, 415, 440, 20);
-            gfx.DrawString("Repasse pela quantidade de conteúdos", font12, XBrushes.Black, rectRepasseQuantidadeTitulo, XStringFormats.TopLeft);
+            gfx.DrawString(texto12, font12, XBrushes.Black, rectRepasseQuantidadeTitulo, XStringFormats.TopLeft);
 
             XRect rectRepasseQuantidade = new XRect(441, 415, 142, 20);
             gfx.DrawString(pdfgrupo.ValorConteudo.ToString("C2", CultureInfo.CreateSpecificCulture(_local)), font12Bold, XBrushes.Black, rectRepasseQuantidade, XStringFormats.TopLeft);
 
             XRect rectRepasseMaisAcessadosTitulo = new XRect(11, 435, 440, 20);
-            gfx.DrawString("Repasse pelas obras de ref e mais acessados", font12, XBrushes.Black, rectRepasseMaisAcessadosTitulo, XStringFormats.TopLeft);
+            gfx.DrawString(texto13, font12, XBrushes.Black, rectRepasseMaisAcessadosTitulo, XStringFormats.TopLeft);
 
             XRect rectRepasseMaisAcessados = new XRect(441, 435, 142, 20);
             gfx.DrawString(pdfgrupo.ValorMaisAcessados.ToString("C2", CultureInfo.CreateSpecificCulture(_local)), font12Bold, XBrushes.Black, rectRepasseMaisAcessados, XStringFormats.TopLeft);
 
             XRect rectRepasseTotalTitulo = new XRect(11, 455, 440, 20);
-            gfx.DrawString("Repasse total", font12, XBrushes.Black, rectRepasseTotalTitulo, XStringFormats.TopLeft);
+            gfx.DrawString(texto14, font12, XBrushes.Black, rectRepasseTotalTitulo, XStringFormats.TopLeft);
 
             XRect rectRepasseTotal = new XRect(441, 455, 142, 20);
             gfx.DrawString(pdfgrupo.ValorTotalRepasse.ToString("C2", CultureInfo.CreateSpecificCulture(_local)), font12Bold, XBrushes.Black, rectRepasseTotal, XStringFormats.TopLeft);
